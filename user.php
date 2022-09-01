08076fc3c8bc9c286760b97358e3eb21ad65fec4
Diraf12 <104466448+Diraf12@users.noreply.github.com>
2022-09-01 13:26:20 +0200

    maj

--- user.php
+++ user.php
@@ -13,12 +13,13 @@
 
 
 </body>
+
 </html>
 
 <?php
 $link = mysqli_connect("localhost", "farid", "", "user");
 $id = $_GET["id"];
-var_dump($_GET);
+//var_dump($_GET);
 $sqli = "SELECT * FROM  user WHERE id = $id";
 $result = mysqli_query($link, $sqli);
 $row = mysqli_fetch_assoc($result);
@@ -27,22 +28,22 @@ $row = mysqli_fetch_assoc($result);
 
 <div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1634804306598-f2efe3ead034?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2938&q=80');
       height: 100vh;">
-      <br>
-        <h1 class="text-center text-white display-1"> <em> Modifier utilisateur </em></h1>
-        <form class="form_user p-5" method="POST" action="up_user.php">
-            <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
-            <div class="form-group ">
-                <label class="text-white display-6" for="email">Email</label>
-                <input type="email" name="email" class="form-control" value="<?php echo htmlspecialchars($row["email"], ENT_QUOTES, 'UTF-8') ?>">
-            </div>
-            <div class="form-group ">
-                <label class="text-white display-6" for="password">Mot de passe</label>
-                <input type="password" name="password" class="form-control mb-3" value="<?php echo htmlspecialchars($row["password"], ENT_QUOTES, 'UTF-8') ?>">
-            </div>
-            <div class="form-group d-flex justify-content-center m-3">
-                <a href="users.php"><input type="button" value="Retour Liste" class="btn btn-primary m-2"></a>
-                <input type="submit" name="modifier" value="Modifier" class="btn btn-success m-2">
-
-            </div>
-        </form>
+  <br>
+  <h1 class="text-center text-white display-1"> <em> Modifier utilisateur </em></h1>
+  <form class="form_user p-5" method="POST" action="up_user.php">
+    <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
+    <div class="form-group ">
+      <label class="text-white display-6" for="email">Email</label>
+      <input type="email" name="email" class="form-control" value="<?php echo htmlspecialchars($row["email"], ENT_QUOTES, 'UTF-8') ?>">
     </div>
+    <div class="form-group ">
+      <label class="text-white display-6" for="password">Mot de passe</label>
+      <input type="password" name="password" class="form-control mb-3" value="<?php echo htmlspecialchars($row["password"], ENT_QUOTES, 'UTF-8') ?>">
+    </div>
+    <div class="form-group d-flex justify-content-center m-3">
+      <a href="users.php"><input type="button" value="Retour Liste" class="btn btn-primary m-2"></a>
+      <input type="submit" name="modifier" value="Modifier" class="btn btn-success m-2">
+
+    </div>
+  </form>
+</div>
--- users.php
+++ users.php
@@ -8,48 +8,53 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
   <title>users</title>
 </head>
+<div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80');
+      height: 100vh;">
+  <br>
 
-<body>
+  <body>
+    <?php
+    $link =  mysqli_connect("localhost", "farid", "", "user");
+    $sql = "SELECT id, nom, prenom, email, message FROM `user`";
+    $results = mysqli_query($link, $sql);
+    ?>
+    <?php echo "<table border = '1px' class='table table-dark'>" ?>
+
+
+    <h1 class="text-center display-1"> <em> Tous les utilisateurs </em> </h1>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">NOM</th>
+          <th scope="col">PRENOM</th>
+          <th scope="col">EMAIL</th>
+          <th scope="col">Message</th>
+        </tr>
+      </thead>
+
+      <?php foreach ($results as $result) : ?>
+        <tbody class="">
+          <tr>
+            <th scope="row"><?php echo $result["id"] ?> </th>
+            <th scope="row"><a class="text-success display-6" href="user.php?id=<?php echo $result["id"] ?>"><?php echo $result["nom"] ?></a></th>
+            <th scope="row"><?php echo $result["prenom"] ?> </th>
+            <th scope="row"><?php echo $result["email"] ?> </th>
+            <th scope="row"><?php echo $result["message"] ?> </th>
+            <td scope="col">
+
+              <button class="btn btn-danger">
+                <a class="text-dark text-decoration-none" href="del_user.php?id= <?php echo $result["id"] ?>"> suprimer</a>
+              </button>
+            </td>
+
+        </tbody>
+
+      <?php endforeach ?>
+
+      <?php echo "</table>" ?>
+
+</div>
 
 </body>
-
 </html>
-<?php
-$link =  mysqli_connect("localhost", "farid", "", "user");
-$sql = "SELECT id, nom, prenom, email, message FROM `user`";
-$results = mysqli_query($link, $sql);
-?>
-<?php echo "<table border = '1px' class='table table-dark'>" ?>
-<h1 class="text-center display-1"> <em> Tous les utilisateurs </em> </h1>
-<table class="table bg-black text-white">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">NOM</th>
-      <th scope="col">PRENOM</th>
-      <th scope="col">EMAIL</th>
-      <th scope="col">Message</th>
-    </tr>
-  </thead>
-
-  <?php foreach ($results as $result) : ?>
-    <tbody class="">
-      <tr>
-        <th scope="row"><?php echo $result["id"] ?> </th>
-        <th scope="row"><a class="text-success" href="user.php?id=<?php echo $result["id"] ?>"><?php echo $result["nom"] ?></a></th>
-        <th scope="row"><?php echo $result["prenom"] ?> </th>
-        <th scope="row"><?php echo $result["email"] ?> </th>
-        <th scope="row"><?php echo $result["message"] ?> </th>
-        <td scope="col">
-          <button class="btn btn-primary ">
-            <a class="text-dark text-decoration-none" href="new_up_user.php?id= <?php echo $result["id"] ?>"> modifier</a>
-          </button>
-
-          <button class="btn btn-danger">
-            <a class="text-dark text-decoration-none" href="del_user.php?id= <?php echo $result["id"] ?>"> suprimer</a>
-          </button>
-        </td>
-    </tbody>
-  <?php endforeach ?>
-
-  <?php echo "</table>" ?>
